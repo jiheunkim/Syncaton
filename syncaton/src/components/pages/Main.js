84d5e936110769b1f500be957fb5919b90b078de
katekim512 <katekim512@konkuk.ac.kr>
2023-10-06 01:05:09 +0900

    메인기능

---
+++ syncaton/src/components/pages/Main.js
@@ -0,0 +1,23 @@
+//import React from 'react';
+import React, { useState } from 'react';
+import '../../App.css';
+import HeroSection from '../HeroSection';
+import Cards from '../Cards';
+// import Footer from '../Footer';
+// import ServiceTable from '../ServiceTable';
+// import Home2 from '../Home2';
+
+
+
+function Main() {
+
+  return (
+      <>
+      <Cards/>
+      
+      </>
+
+  );
+}
+
+export default Main;
