05fddcfbde095095ec3f10e0455231b660dc0468
jiheunkim <ziheun54@gmail.com>
2023-10-09 17:44:36 +0900

    HeroSection: 메인배경 넣기

--- syncaton/src/components/HeroSection.js
+++ syncaton/src/components/HeroSection.js
@@ -10,20 +10,14 @@ function HeroSection() {
 
     return (
         <div className = 'hero-container'>
-            <img src="/image/main-bg.png"/>
-            {/* <video src="/videos/twinkle.mp4" autoPlay loop muted /> */}
-            <h1>Syncaton</h1>
-            <p style={{ fontSize: '14px' }}>시대의 흐름에 따른 초개인화 서비스</p>
-            <p>금융소비자 테스트</p>
-
+            <img src="/image/mainbg.jpg"/>
             <br></br>
-            <div className="hero-btns">
+            {/* <div className="hero-btns">
                 <Button className='btns' buttonStyle='btn--outline'
                 buttonSize='btn--large' to="/main">GET STARTED</Button>
                 <Button className = 'btns' buttonStyle = 'btn--primary' 
                 buttonSize = 'btn--large'>WATCH TRAILER<i className = 'far fa-play-circle'/></Button>
-            </div>
-               
+            </div> */}
         </div>
     )
 }
