d36a6e4a1150549a523161fbca1061cda8901604
katekim512 <katekim512@konkuk.ac.kr>
2023-10-10 12:22:13 +0900

    막대바 생성

---
+++ syncaton/src/components/BarChart.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './BarChart.css'; // 스타일 파일을 불러옵니다.
+
+
+const BarChart = ({ value,index,fields }) => {
+  const barStyle = {
+    width: `${value}px`, // 숫자값에 따라 막대의 넓이를 동적으로 설정합니다.
+  };
+
+  return (
+    <div className="bar-container">
+      <div className="bar" style={barStyle}></div>
+      <p className="bar-text">{fields[index]}에 {value}% 지출하셨습니다.</p> {/* 여기에서 index를 사용해야 올바른 설명이 표시됩니다. */}
+    </div>
+  );
+};
+
+export default BarChart;
--- syncaton/src/components/Mbti.js
+++ syncaton/src/components/Mbti.js
@@ -1,11 +1,38 @@
-import React from 'react';
 import './Mbti.css'; // 스타일 파일 import
 import CardItem from './CardItem';
+import BarChart from './BarChart'; // BarChart 컴포넌트를 불러옵니다.
+import React, { useState, useEffect } from 'react';
 
 const MAX_SUMMARY_LENGTH = 100;
 let message = 'message';
 
 const Mbti = ({ item, isFirst }) => {
+  const [values, setValues] = useState([50, 120, 80]);
+  const [fields, setFields] = useState(['첫 번째 분야', '두 번째 분야', '세 번째 분야']);
+  useEffect(() => {
+    // 백엔드 API 엔드포인트 URL
+    const apiUrl = '백엔드 API 엔드포인트 URL을 입력하세요';//백엔드 API!!!!!!!!!!!!!!!
+
+    // 백엔드에서 데이터를 비동기적으로 가져오는 함수를 정의합니다.
+    const fetchData = async () => {
+      try {
+        // API 요청 보내기
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        // 받아온 데이터로 values와 texts 상태를 업데이트합니다.
+        setValues(data.values);
+        setFields(data.fields);
+      } catch (error) {
+        console.error('데이터 가져오기 실패:', error);
+      }
+    };
+
+    // 컴포넌트가 마운트될 때 데이터를 가져오도록 호출합니다.
+    fetchData();
+  }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 설정합니다.
+
+
+
   return (
     <div className='mbti-container'>
       <div className='mbti-content'>
@@ -22,6 +49,17 @@ const Mbti = ({ item, isFirst }) => {
             <br></br> {/* 요약 내용 표시 */}
           </span>
         </div>
+        <br></br><br></br><br></br>
+
+        <div className="bar-chart-container">
+  {values.map((value, index) => (
+    <BarChart key={index} value={value} index={index} fields={fields}/>
+  ))}
+</div>
+
+
+    <br></br><br></br><br></br>
+
         <div className='mbti-image'>
         <CardItem
               src='https://slownews.kr/wp-content/uploads/2016/11/shopping.jpg'
