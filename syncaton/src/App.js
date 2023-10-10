91e27e4081896cb76710f1fb43422e7145b42793
jiheunkim <ziheun54@gmail.com>
2023-10-10 11:10:31 +0900

    Mypage 추가

--- syncaton/src/App.js
+++ syncaton/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import Main from './components/pages/Main';
+import Mypage from './components/pages/Mypage';
 import Mbti from './components/Mbti';
 
 function App() {
@@ -15,7 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/main" element={<Main />} />
         <Route path="/mbti" element={<Mbti />} />
-
+        <Route path="/mypage" element={<Mypage />} />
       </Routes>
     </BrowserRouter>
   );
