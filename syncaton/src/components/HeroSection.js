15edf568d7ff8f8e7f5cf99c85243ed57e071827
katekim512 <katekim512@konkuk.ac.kr>
2023-10-10 12:20:59 +0900

    버튼 생성

--- syncaton/src/components/HeroSection.js
+++ syncaton/src/components/HeroSection.js
@@ -4,13 +4,14 @@ import { Button } from './Button'
 import './HeroSection.css';
 import { useState } from 'react';
 import {Link} from 'react-router-dom';
+import RoundButton from './RoundButton';
 
 
 function HeroSection() {
 
     return (
         <div className = 'hero-container'>
-            <img src="/image/mainbg.jpg"/>
+            <img src="/image/mainbg.jpg"alt="Background"/>
             <br></br>
             {/* <div className="hero-btns">
                 <Button className='btns' buttonStyle='btn--outline'
@@ -18,6 +19,20 @@ function HeroSection() {
                 <Button className = 'btns' buttonStyle = 'btn--primary' 
                 buttonSize = 'btn--large'>WATCH TRAILER<i className = 'far fa-play-circle'/></Button>
             </div> */}
+
+            <br></br><br></br><br></br><br></br>
+
+            <div className="hero-btns">
+                <RoundButton className='btns' buttonStyle='btn--outline'
+                buttonSize='btn--large' to="/main">내 소비캐릭터 확인하기</RoundButton>
+            </div>
+
+            
+            {/* <Link to="/your-route">
+            <img src="./buttonimg.png" alt="Button"
+            style={{ width: `50px`, height: '20px' }}/>
+            </Link> */}
+            
         </div>
     )
 }
