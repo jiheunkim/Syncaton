dcdad5216e2fb8e3d0a90bae38554846e98843f0
katekim512 <katekim512@konkuk.ac.kr>
2023-10-11 22:12:11 +0900

    백앤드 통신 부분 수정

--- syncaton/src/components/Mbti.js
+++ syncaton/src/components/Mbti.js
@@ -8,7 +8,7 @@ let message = 'message';
 
 const Mbti = ({ item, isFirst }) => {
   const [values, setValues] = useState([50, 120, 80]);
-  const [fields, setFields] = useState(['첫 번째 분야', '두 번째 분야', '세 번째 분야']);
+  const [indexs, setIndexs] = useState([4,5,6]);
   useEffect(() => {
     // 백엔드 API 엔드포인트 URL
     const apiUrl = 'https://6097350120.for-seoul.synctreengine.com/account'; // 백엔드 API 주소
@@ -21,7 +21,7 @@ const Mbti = ({ item, isFirst }) => {
         const data = await response.json();
         // 받아온 데이터로 values와 fields 상태를 업데이트합니다.
         setValues(data.data.result.map(item => parseInt(item[1], 10))); // 숫자로 변환하여 저장
-        setFields(data.data.result.map(item => item[0])); // 텍스트로 저장
+        setIndexs(data.data.result.map(item => item[0])); // 텍스트로 저장
       } catch (error) {
         console.error('데이터 가져오기 실패:', error);
       }
@@ -33,27 +33,55 @@ const Mbti = ({ item, isFirst }) => {
 
 
 
+
+  const [nickname, setNickname] = useState(['쇼핑매니아']);
+  const [explain, setExplain] = useState(['당신은 재미있는 쇼핑을 즐기며 지갑과 마음 모두에 행복을 담는 쇼핑 매니아 입니다.']);
+  useEffect(() => {
+    // 백엔드 API 엔드포인트 URL
+    const apiUrl = 'https://6097350120.for-seoul.synctreengine.com/account'; // 백엔드 API 주소
+
+    // 백엔드에서 데이터를 비동기적으로 가져오는 함수를 정의합니다.
+    const fetchData = async () => {
+      try {
+        // API 요청 보내기
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+        // 받아온 데이터로 values와 fields 상태를 업데이트합니다.
+        setNickname(data.data.result.map(item => item[0])); // 숫자로 변환하여 저장
+        setExplain(data.data.result.map(item => item[1])); // 텍스트로 저장
+      } catch (error) {
+        console.error('데이터 가져오기 실패:', error);
+      }
+    };
+
+    // 컴포넌트가 마운트될 때 데이터를 가져오도록 호출합니다.
+    fetchData();
+  }, []); // 빈 배열을 전달하여 컴포넌트가 마운트될 때 한 번만 실행되도록 설정합니다.
+
+
+
+
   return (
     <div className='mbti-container'>
       <div className='mbti-content'>
         <div className='mbti-text'>
-          <span className='w-btn w-btn-blue' style={{ fontWeight: 'bold',fontSize:'30px' }}>당신의 쇼핑유형은 "쇼핑매니아"입니다.</span>
+          <span className='w-btn w-btn-blue' style={{ fontWeight: 'bold',fontSize:'30px' }}>당신의 쇼핑유형은 "{nickname}"입니다.</span>
           <br></br>
           <span style={{ fontWeight: 600, fontSize: '20px' }}>{}</span>
           <br />
           <span style={{ fontWeight: 300, fontSize: '16px', lineHeight: '1.5' }}>
-            '당신은 재미있는 쇼핑을 즐기며 지갑과 마음 모두에 행복을 담는 쇼핑 매니아 입니다.' <br></br><br></br>
-             쇼핑에 90%를 지출했습니다. <br></br>
+            {explain} <br></br><br></br>
+             {/* 쇼핑에 90%를 지출했습니다. <br></br>
              의류에 65%를 지출했습니다.<br></br>
-             현대백화점에 70%를 지출하였습니다.<br></br>
+             현대백화점에 70%를 지출하였습니다.<br></br> */}
             <br></br> {/* 요약 내용 표시 */}
           </span>
         </div>
         <br></br><br></br><br></br>
 
         <div className="bar-chart-container">
-  {values.map((value, index) => (
-    <BarChart key={index} value={value} index={index} fields={fields}/>
+  {values.map((value, indexs) => (
+    <BarChart key={indexs} value={value} indexs={indexs}/>
   ))}
 </div>
 
